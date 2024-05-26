756a2b76ea952673b2359466ccd16ddad4621e46
Annie Huang <annie.hn@gmail.com>
2024-05-26 16:43:36 +1000

    Update files directly under /src

--- growlers/src/App.tsx
+++ growlers/src/App.tsx
@@ -1,19 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { SimpleGrid, Text, ChakraProvider } from '@chakra-ui/react';
 
-import './index.css'
+import Search from './components/Search';
+import Taps from './components/Taps';
+import Cart from './components/Cart';
+
+import './index.css';
+
+const H3 = ({ children }) => (
+  <Text fontSize='xl' mb={3} fontWeight='bold' textAlign='center'>
+    {children}
+  </Text>
+);
 
 const App = () => (
-  <div className="container">
-    <div>Name: growlers</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Empty CSS</div>
-  </div>
-)
-const rootElement = document.getElementById('app')
-if (!rootElement) throw new Error('Failed to find the root element')
+  <ChakraProvider>
+    <SimpleGrid
+      columns={[1, 1, 3]}
+      spacing={10}
+      m={[1, 1, 6]}
+      templateColumns='1fr 3fr 1fr'
+      gap={1}
+    >
+      <div>
+        <H3>Search</H3>
+        <Search />
+      </div>
+      <div>
+        <H3>Taps</H3>
+        <Taps />
+      </div>
+      <div>
+        <H3>Cart</H3>
+        <Cart />
+      </div>
+    </SimpleGrid>
+  </ChakraProvider>
+);
 
-const root = ReactDOM.createRoot(rootElement as HTMLElement)
+const rootElement = document.getElementById('app');
+if (!rootElement) throw new Error('Failed to find the root element');
 
-root.render(<App />)
+const root = ReactDOM.createRoot(rootElement as HTMLElement);
+
+root.render(<App />);
