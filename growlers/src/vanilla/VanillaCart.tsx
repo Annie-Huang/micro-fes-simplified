15f8277ae7dbc3d83aa894daed9b6320013bdea6
Annie Huang <annie.hn@gmail.com>
2024-05-29 17:26:35 +1000

    Clean up the whole content and just keep the <Cart> inside <VanillaCart>

--- growlers/src/vanilla/VanillaCart.tsx
+++ growlers/src/vanilla/VanillaCart.tsx
@@ -1,45 +1,12 @@
 import React, { FC } from 'react';
 import ReactDOM from 'react-dom/client';
-import { SimpleGrid, Text, ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 
-// Load the store.
-import { load } from './store';
-load('hv-taplist');
-
-import Search from './components/Search';
-import Taps from './components/Taps';
 import Cart from './components/Cart';
-
-import './index.css';
-
-const H3: FC<React.PropsWithChildren> = ({ children }) => (
-  <Text fontSize='xl' mb={3} fontWeight='bold' textAlign='center'>
-    {children}
-  </Text>
-);
 
 const App = () => (
   <ChakraProvider>
-    <SimpleGrid
-      columns={[1, 1, 3]}
-      spacing={10}
-      m={[1, 1, 6]}
-      templateColumns='1fr 3fr 1fr'
-      gap={1}
-    >
-      <div>
-        <H3>Search</H3>
-        <Search />
-      </div>
-      <div>
-        <H3>Taps</H3>
-        <Taps />
-      </div>
-      <div>
-        <H3>Cart</H3>
-        <Cart />
-      </div>
-    </SimpleGrid>
+    <Cart />
   </ChakraProvider>
 );
 
