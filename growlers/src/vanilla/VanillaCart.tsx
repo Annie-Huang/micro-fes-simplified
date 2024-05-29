eb52561e55be5dc04f6032989b33f950bfbcdd6c
Annie Huang <annie.hn@gmail.com>
2024-05-29 17:30:22 +1000

    Create a function to do document.querySelector(selector) from export default for <VanillaCart>

--- growlers/src/vanilla/VanillaCart.tsx
+++ growlers/src/vanilla/VanillaCart.tsx
@@ -10,9 +10,20 @@ const App = () => (
   </ChakraProvider>
 );
 
+/*
 const rootElement = document.getElementById('app');
 if (!rootElement) throw new Error('Failed to find the root element');
 
 const root = ReactDOM.createRoot(rootElement as HTMLElement);
 
 root.render(<App />);
+*/
+
+export default (selector: string) => {
+  const rootElement = document.querySelector(selector);
+  if (!rootElement) throw new Error('Failed to find the root element');
+
+  const root = ReactDOM.createRoot(rootElement as HTMLElement);
+
+  root.render(<App />);
+};
