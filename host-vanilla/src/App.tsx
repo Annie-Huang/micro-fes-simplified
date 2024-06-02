d97cfa6bfeac720112b398fbcfc23b118e9c9318
Annie Huang <annie.hn@gmail.com>
2024-06-02 13:33:16 +1000

    Replace <App> in host-vanilla to load the 'hv-taplist' json

--- host-vanilla/src/App.tsx
+++ host-vanilla/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+/*import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
@@ -16,4 +16,13 @@ if (!rootElement) throw new Error('Failed to find the root element')
 
 const root = ReactDOM.createRoot(rootElement as HTMLElement)
 
-root.render(<App />)
+root.render(<App />)*/
+
+import './index.css';
+
+import createTaps from 'growlers/VanillaTaps';
+import createCart from 'growlers/VanillaCart';
+import createSearch from 'growlers/VanillaSearch';
+import { load, subscribe } from 'growlers/store';
+
+load('hv-taplist');
