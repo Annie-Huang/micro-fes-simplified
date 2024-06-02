214102079cfb5fb004686ad9d78c8ec6b1f40313
Annie Huang <annie.hn@gmail.com>
2024-06-02 16:20:18 +1000

    Update the <DataComponent> to wrapped store state from children

--- growlers/src/components/DataComponent.tsx
+++ growlers/src/components/DataComponent.tsx
@@ -1,40 +1,13 @@
-import React from 'react';
-import { Box, Button, Text } from '@chakra-ui/react';
+import React, { ReactElement } from 'react';
 import { useSnapshot } from 'valtio';
 
-import { MFE_BORDER } from '../constants';
-import store from '../store';
+import store, { TapStore } from '../store';
 
-const Cart = () => {
-  const { cart } = useSnapshot(store);
-
-  return (
-    <Box border={MFE_BORDER}>
-      {cart.map((beverage) => (
-        <Box
-          borderBottom='1px'
-          borderBottomColor='gray.200'
-          mb={3}
-          key={[beverage.producerName, beverage.beverageName].join('')}
-        >
-          <Box>
-            <Text fontSize='2xl'>
-              <strong>{beverage.producerName}</strong>&nbsp;
-              {beverage.beverageName}
-            </Text>
-          </Box>
-          <Box>
-            <Text>{beverage.beverageStyle}</Text>
-          </Box>
-        </Box>
-      ))}
-      <Box>
-        <Button width='100%' onClick={() => (store.cart = [])}>
-          Checkout
-        </Button>
-      </Box>
-    </Box>
-  );
+const DataComponent: React.FC<{
+  children: (state: TapStore) => ReactElement<any, any>;
+}> = ({ children }) => {
+  const state = useSnapshot(store);
+  return children(state);
 };
 
-export default Cart;
+export default DataComponent;
